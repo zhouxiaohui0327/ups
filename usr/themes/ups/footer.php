f9d64b80075d5afcd3784ea90311e1ffe7000eb1
zhouxiaohui0327 <475503798@qq.com>
2016-01-11 16:49:32 +0800

    modify

--- usr/themes/ups/footer.php
+++ usr/themes/ups/footer.php
@@ -5,10 +5,10 @@
 </div><!-- end #body -->
 
 <footer>
-    <p><a href="/index.php/contact.html">关于我们</a>|<a href="/index.php/server.html">维修服务</a>|<a href="">企业邮箱</a></p>
-    <p>版权所有 PEAKPOWER PEAK POWER 杭州君轩科技有限公司</p>
+    <p><a href="/index.php/contact.html">关于我们</a>|<a href="/index.php/server.html">维修服务</a>|<a href="http://mail.junxuan.co">企业邮箱</a></p>
+    <p>版权所有  杭州君轩科技有限公司</p>
     <p>地 址：杭州市西湖区振华路196号裕华大厦B座1712室 电 话：0571-85358995</p>
-    <p>传 真：0571-88305660  邮 箱：xxx@xxx.com</p>
+    <p>传 真：0571-88305660</p>
 </footer><!-- end #footer -->
 
 <?php $this->footer(); ?>
